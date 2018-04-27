19fa99f67859cd31726f54a89a416a18920de3a5
10086 <935593801@qq.com>
2018-04-27 09:01:30 +0800

    update

---
+++ start_project/library/imports/82/82c2fb9b-6d2d-4395-be1f-d2d636f7ea78.js
@@ -0,0 +1,78 @@
+"use strict";
+cc._RF.push(module, '82c2fubbS1Dlb4f0tY29+p4', 'Star');
+// NewScript/Star.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        pickRadius: 0,
+        // 暂存 Game 对象的引用
+        game: {
+            default: null,
+            serializable: false // },
+        } },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    getPlayerDistance: function getPlayerDistance() {
+        // 根据 player 节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        // 根据两点位置计算两点之间距离
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+
+    onPicked: function onPicked() {
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+        this.game.spawnNewStar();
+        // 调用 Game 脚本的得分方法
+        this.game.gainScore();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+    },
+    // onLoad () {},
+
+    start: function start() {},
+
+
+    update: function update(dt) {
+        // 每帧判断和主角之间的距离是否小于收集距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // 调用收集行为
+            this.onPicked();
+            return;
+        }
+    } // update (dt) {},
+});
+
+cc._RF.pop();
